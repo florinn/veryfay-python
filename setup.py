402f7c1b73b62e340c084f6ac80a2f9cad81baec
florinn <florinn>
2015-10-13 21:47:39 +0300

    update test requires

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'veryfay',
     version = '0.1.0',
-    tests_require=["behave>=1.2.4"],
+    tests_require=["behave>=1.2.4", "pyhamcrest>=1.8.5"],
 	packages=['veryfay'],
 	description = 'A library for activity based authorization.',
     author = 'Florin Nitoi',
