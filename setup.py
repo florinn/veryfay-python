81fd8d6ef4dd358f407aea8e36a084d12c921832
florinn <florinn>
2015-10-14 17:10:42 +0300

    hk

--- setup.py
+++ setup.py
@@ -1,47 +1,42 @@
-import codecs
-import os
-import re
-
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 ####################################################################
 
-NAME = "veryfay"
-VERSION = "0.1.0"
-DESCRIPTION = "Activity based authorization."
-URL = "https://github.com/florinn/veryfay-python"
-AUTHOR = "Florin Nitoi"
-EMAIL = "florin.nitoi@gmail.com"
-LICENSE = "MIT"
-PACKAGES = ['veryfay']
-META_PATH = os.path.join("veryfay", "__init__.py")
-KEYWORDS = ["authorization", "engine", "activity", "role", "permission"]
+NAME = 'veryfay'
+VERSION = '0.1.0'
+DESCRIPTION = 'Activity based authorization.'
+URL = 'https://github.com/florinn/veryfay-python'
+AUTHOR = 'Florin Nitoi'
+EMAIL = 'florin.nitoi@gmail.com'
+LICENSE = 'MIT'
+PACKAGES = [NAME]
+KEYWORDS = ['authorization', 'engine', 'activity', 'role', 'permission']
 CLASSIFIERS = [
-    "Development Status :: 5 - Production/Stable",
-    "Intended Audience :: Developers",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "License :: OSI Approved :: MIT License",
-    "Programming Language :: Python :: 3.3",
-    "Programming Language :: Python :: 3.4",
-    "Programming Language :: Python :: 3.5",
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
 ]
 INSTALL_REQUIRES = []
-TEST_REQUIRES = ["behave>=1.2.4", "pyhamcrest>=1.8.5"]
+TEST_REQUIRES = ['behave>=1.2.4', 'pyhamcrest>=1.8.5']
 
 ###################################################################
 
 setup(
     name=NAME,
-	version=VERSION,
+    version=VERSION,
     description=DESCRIPTION,
-	long_description=DESCRIPTION,
+    long_description=DESCRIPTION,
     packages=PACKAGES,
     zip_safe=False,
     classifiers=CLASSIFIERS,
     install_requires=INSTALL_REQUIRES,
     tests_require=TEST_REQUIRES,
-	license=LICENSE,
+    license=LICENSE,
     url=URL,
     author=AUTHOR,
     author_email=EMAIL,
